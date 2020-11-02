85c80af682d85f3d4efe93593ba1c0962938246b
Joshua Unseen <corpselight@gmail.com>
2020-11-01 21:52:04 -0500

    Remembering some technique...

--- cupcakes.js
+++ cupcakes.js
@@ -1,11 +1,11 @@
 let order = {
     cakes: {},
+    name: "no data entered",
     address: "no data entered",
     city: "no data entered",
     zip: "no data entered",
     phone: "no data entered"
 }
-let priceTotal = 0;
 
 const cakeSelect = document.getElementById("cake-select");
 const custInfo = document.getElementById("customer-info");
@@ -13,17 +13,27 @@ const custInfo = document.getElementById("customer-info");
 cakeSelect.addEventListener("input", CakeTotal);
 custInfo.addEventListener("input", InfoDump);
 
-function CakeTotal() {
+function CakeTotal(event) {
+    order.cakes[event.target.name] = parseInt(event.target.value);
+    GoAhead();
+
+}
+
+function InfoDump(event) {
+    order[event.target.name] = event.target.value;
+
+    GoAhead();
+}
+
+function GoAhead() {
     const payButtons = document.getElementById("submit");
     let cakes = 0;
     let info = false;
-    // for (let i = 1; i < 7; i++) {
-    //     cakes += parseInt(document.getElementById("cake"+i).children[0].value);
-    // }
+
     for (const field of cakeSelect.getElementsByTagName("input")) {
-        console.log(cakeSelect);
         cakes += parseInt(field.value);
     }
+    console.log(order.cakes);
     for (const field of custInfo.getElementsByTagName("input")) {
         console.log(field.value)
         if (field.value === "") {
@@ -38,29 +48,12 @@ function CakeTotal() {
     else {
         payButtons.disabled = true;
     }
-    priceTotal = cakes * 3;
-    console.log(priceTotal);
-}
-
-function InfoDump(event) {
-    console.log(event.target.value);
-    order[event.target.name] = event.target.value;
-}
-
-function EmptyCheck(cakes, info) {
+    order.price = cakes * 3;
+    console.log(order.price);
 
 }
 
 function Pay(payMethod) {
-    // document.getElementById("cake-select").disabled = true;
-    for (let i = 1; i < 7; i++) {
-        let thisCake = document.getElementById("cake" + i).children;
-        let cakeName = thisCake[1].innerText;
-        let cakeQuantity = thisCake[0].value;
-        if (cakeQuantity > 0) {
-            order.cakes[cakeName] = cakeQuantity;
-        }
-    }
     console.log(order);
     switch (payMethod) {
         case "paypal":
